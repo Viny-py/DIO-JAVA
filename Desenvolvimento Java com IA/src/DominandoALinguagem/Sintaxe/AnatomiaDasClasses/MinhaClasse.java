92b031a434d821db591fe7a8efe175c2f19321ab
Vinicius <vviinyf@gmail.com>
2024-06-22 12:14:51 -0400

    Salvando e organizando Files de acordo com suas aulas respectivas

---
+++ Desenvolvimento Java com IA/src/DominandoALinguagem/Sintaxe/AnatomiaDasClasses/MinhaClasse.java
@@ -0,0 +1,17 @@
+package DominandoALinguagem.Sintaxe.AnatomiaDasClasses;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String firstName = "Viny";
+        String lastName = "Fonseca Oliveira";
+
+        String fullName = nomeCompleto(firstName, lastName);
+
+        System.out.println(fullName);
+    }
+
+    public static String nomeCompleto (String firstName, String lastName) {
+        return "Meu nome: ".concat(firstName).concat(" ").concat(lastName);
+    }
+    
+}
